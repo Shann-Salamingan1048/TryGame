a42b52bf161345be23477eed85a533fc5c90bddd
Shann Delfin C. Salamingan IIII <salaminganshann@gmail.com>
2024-05-13 10:12:30 +0800

    TryGame Java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println("hi");
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
